55eaf8b6bf9cf11c84d92d865eac156dd748e3eb
mariusfa <mariusfagerland@yahoo.no>
2022-11-30 20:24:03 +0100

    Parse args

---
+++ src/cli/mod.rs
@@ -0,0 +1,32 @@
+mod help;
+
+#[derive(Debug, PartialEq)]
+pub enum Action {
+    Start,
+    Status,
+    Help,
+    TooManyArgs,
+}
+
+pub fn parse_args(args: &Vec<String>) -> Action {
+    if args.len() == 1 {
+        return Action::Help;
+    } else if args.len() == 2 {
+        return match args[1].as_str() {
+            "start" => Action::Start,
+            "status" => Action::Status,
+            _ => Action::Help,
+        };
+    } else {
+        return Action::TooManyArgs;
+    }
+}
+
+pub fn perform_action(action: Action) {
+    match action {
+        Action::Help => help::help(),
+        Action::TooManyArgs => println!("Too many args"),
+        Action::Start => println!("Start"),
+        Action::Status => println!("Status"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 use std::env;
 
-mod help;
+mod cli;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
-    if args.len() == 1 {
-        help::help();
-    } else if args.len() == 2 {
-        print!("Do actions");
-    } else {
-        print!("Too many args");
-    }
+    let action = cli::parse_args(&args);
+    cli::perform_action(action);
 }
