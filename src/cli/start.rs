da91733236f425f36d70e29c80cd0bf1f9cdee0a
mariusfa <mariusfagerland@yahoo.no>
2022-12-01 19:20:21 +0100

    Refactor

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -1,11 +1,15 @@
-use std::fs::{create_dir_all, File};
+use std::{
+    error::Error,
+    fs::{create_dir_all, File},
+    io,
+};
 
 use dirs::home_dir;
 
 pub fn start() {
     println!("Start");
     let file_path = get_file_path();
-    create_if_not_exist(file_path);
+    create_if_not_exist(file_path).expect("Failed to create file");
 }
 
 fn get_file_path() -> std::path::PathBuf {
@@ -14,15 +18,15 @@ fn get_file_path() -> std::path::PathBuf {
     file_path
 }
 
-fn create_if_not_exist(file_path: std::path::PathBuf) {
+fn create_if_not_exist(file_path: std::path::PathBuf) -> io::Result<()> {
     if !file_path.exists() {
         println!("time.txt not found. Creating time.txt...");
         create_dir_all(
             file_path
                 .parent()
-                .expect("Failed to get parent of file path"),
-        )
-        .expect("Error creating dirs");
-        File::create(file_path).expect("Error creating file");
+                .expect("Failed to get parent directory from file path"),
+        )?;
+        File::create(file_path)?;
     }
+    Ok(())
 }
