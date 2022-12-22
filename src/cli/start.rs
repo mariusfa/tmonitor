494fafdc6ddd9dd35487620de025e3b6c8636fbb
mariusfa <mariusfagerland@yahoo.no>
2022-12-22 20:33:31 +0100

    Make it work

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -1,3 +1,4 @@
+use core::time;
 use std::fs::{self, create_dir_all, File};
 
 use chrono::{Local, TimeZone};
@@ -8,62 +9,56 @@ pub fn start() {
     let file_path = get_file_path();
     create_if_not_exist(&file_path).expect("Failed to create file");
 
-    let mut contents = fs::read_to_string(&file_path).expect("read failed");
+    let contents = fs::read_to_string(&file_path).expect("read failed");
 
     if contents.len() == 0 {
         set_first_timestamp(file_path);
     } else {
-        fs::write(file_path, format!("fuck")).expect("oops")
+        let mut lines: Vec<&str> = contents.split("\n").collect();
+        let lines_length = lines.len();
+
+        let mut last_line = lines[lines_length - 1];
+        let timestamps_last_line: Vec<&str> = last_line.split_whitespace().collect();
+        let first_timestamp = timestamps_last_line[0];
+        let last_timestamp = timestamps_last_line[1];
+
+        let now_timestamp = Local::now().timestamp();
+        let now_date = Local
+            .timestamp_opt(now_timestamp, 0)
+            .single()
+            .unwrap()
+            .format("%d %m %Y")
+            .to_string();
+
+        let last_timestamp: i64 = last_timestamp.parse().unwrap();
+        let last_date = Local
+            .timestamp_opt(last_timestamp, 0)
+            .unwrap()
+            .format("%d %m %Y")
+            .to_string();
+
+        if last_date == now_date {
+            let mut updated_line: String = first_timestamp.to_string();
+            updated_line.push_str(" ");
+            updated_line.push_str(now_timestamp.to_string().as_str());
+
+            last_line = updated_line.as_str();
+
+            lines[lines_length - 1] = last_line;
+            let updated = lines.join("\n");
+            fs::write(file_path, updated).expect("oops");
+        } else {
+            let timestamp = Local::now().timestamp().to_string();
+            let timestamp = timestamp.as_str();
+            let mut new_line = String::new();
+            new_line.push_str(timestamp);
+            new_line.push_str(" ");
+            new_line.push_str(timestamp);
+            lines.push(new_line.as_str());
+            let updated = lines.join("\n");
+            fs::write(file_path, updated).expect("oops");
+        }
     }
-
-    //     println!("{contents}");
-    //     let mut timestamp_entries: Vec<&str> = contents.split("\n").collect();
-    //     let num_timestamp_entries = timestamp_entries.len();
-
-    //     let now = Local::now().timestamp();
-    //     let now_date = Local
-    //         .timestamp_opt(now, 0)
-    //         .single()
-    //         .unwrap()
-    //         .format("%d %m %Y")
-    //         .to_string();
-
-    //     let start_time = timestamp_entries
-    //         .last()
-    //         .unwrap()
-    //         .split_whitespace()
-    //         .next()
-    //         .unwrap();
-
-    //     let last_entry = timestamp_entries
-    //         .last()
-    //         .unwrap()
-    //         .split_whitespace()
-    //         .last()
-    //         .unwrap();
-
-    //     let last_entry: i64 = last_entry.parse().unwrap();
-
-    //     let last_date = Local
-    //         .timestamp_opt(last_entry, 0)
-    //         .single()
-    //         .unwrap()
-    //         .format("%d %m %Y")
-    //         .to_string();
-
-    //     println!("{now_date}");
-    //     println!("{last_date}");
-    //     let is_equal = now_date == last_date;
-    //     println!("{is_equal}");
-
-    //     if is_equal {
-    //         println!("{start_time} {now}");
-    //         timestamp_entries[num_timestamp_entries - 1] = format!("{start_time} {now}");
-    //     } else {
-    //     }
-
-    //     let a = timestamp_entries.join("\n");
-    //     //println!("{num_timestamp_entries}")
 }
 
 fn get_file_path() -> std::path::PathBuf {
