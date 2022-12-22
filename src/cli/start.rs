b965f111ad07594e68968759c008c3d4d93a4dbd
mariusfa <mariusfagerland@yahoo.no>
2022-12-22 21:18:39 +0100

    Refactor

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -1,8 +1,6 @@
-use core::time;
-use std::fs::{self, create_dir_all, File};
-
 use chrono::{Local, TimeZone};
 use dirs::home_dir;
+use std::fs::{self, create_dir_all, File};
 
 pub fn start() {
     println!("Start");
@@ -12,51 +10,25 @@ pub fn start() {
     let contents = fs::read_to_string(&file_path).expect("read failed");
 
     if contents.len() == 0 {
-        set_first_timestamp(file_path);
+        set_first_timestamp(&file_path);
     } else {
         let mut lines: Vec<&str> = contents.split("\n").collect();
-        let lines_length = lines.len();
-
-        let mut last_line = lines[lines_length - 1];
-        let timestamps_last_line: Vec<&str> = last_line.split_whitespace().collect();
-        let first_timestamp = timestamps_last_line[0];
-        let last_timestamp = timestamps_last_line[1];
+        let last_line_index = lines.len() - 1;
+        let (first, second) = extract_timstamps_from_line(lines[last_line_index]);
 
         let now_timestamp = Local::now().timestamp();
-        let now_date = Local
-            .timestamp_opt(now_timestamp, 0)
-            .single()
-            .unwrap()
-            .format("%d %m %Y")
-            .to_string();
 
-        let last_timestamp: i64 = last_timestamp.parse().unwrap();
-        let last_date = Local
-            .timestamp_opt(last_timestamp, 0)
-            .unwrap()
-            .format("%d %m %Y")
-            .to_string();
-
-        if last_date == now_date {
-            let mut updated_line: String = first_timestamp.to_string();
-            updated_line.push_str(" ");
-            updated_line.push_str(now_timestamp.to_string().as_str());
-
-            last_line = updated_line.as_str();
-
-            lines[lines_length - 1] = last_line;
-            let updated = lines.join("\n");
-            fs::write(file_path, updated).expect("oops");
+        if is_today(second, now_timestamp) {
+            let updated_line = create_line(first, now_timestamp.to_string().as_str());
+            lines[last_line_index] = updated_line.as_str();
+            fs::write(file_path, lines.join("\n")).expect("oops");
         } else {
-            let timestamp = Local::now().timestamp().to_string();
-            let timestamp = timestamp.as_str();
-            let mut new_line = String::new();
-            new_line.push_str(timestamp);
-            new_line.push_str(" ");
-            new_line.push_str(timestamp);
+            let new_line = create_line(
+                now_timestamp.to_string().as_str(),
+                now_timestamp.to_string().as_str(),
+            );
             lines.push(new_line.as_str());
-            let updated = lines.join("\n");
-            fs::write(file_path, updated).expect("oops");
+            fs::write(file_path, lines.join("\n")).expect("oops");
         }
     }
 }
@@ -72,11 +44,46 @@ fn create_if_not_exist(file_path: &std::path::PathBuf) -> Result<(), Box<dyn std
         println!("time.txt not found. Creating time.txt...");
         create_dir_all(file_path.parent().ok_or("Find parent dir failed")?)?;
         File::create(file_path)?;
+    } else {
+        println!("time.txt found. Updateing time.txt...");
     }
     Ok(())
 }
 
-fn set_first_timestamp(file_path: std::path::PathBuf) {
+fn set_first_timestamp(file_path: &std::path::PathBuf) {
     let timestamp = Local::now().timestamp();
     fs::write(file_path, format!("{timestamp} {timestamp}")).expect("oops")
+}
+
+fn extract_timstamps_from_line(line: &str) -> (&str, &str) {
+    let timestamps_last_line: Vec<&str> = line.split_whitespace().collect();
+    let first = timestamps_last_line[0];
+    let second = timestamps_last_line[1];
+    return (first, second);
+}
+
+fn is_today(last_timestamp: &str, now_timestamp: i64) -> bool {
+    let last_timestamp: i64 = last_timestamp.parse().unwrap();
+    let last_date = Local
+        .timestamp_opt(last_timestamp, 0)
+        .unwrap()
+        .format("%d %m %Y")
+        .to_string();
+
+    let now_date = Local
+        .timestamp_opt(now_timestamp, 0)
+        .single()
+        .unwrap()
+        .format("%d %m %Y")
+        .to_string();
+
+    return last_date == now_date;
+}
+
+fn create_line(first: &str, second: &str) -> String {
+    let mut line = String::new();
+    line.push_str(first);
+    line.push_str(" ");
+    line.push_str(second);
+    return line;
 }
