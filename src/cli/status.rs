5113e2285491c15da3076bb2f218e51cd063c1e4
mariusfa <mariusfagerland@yahoo.no>
2023-01-02 21:48:57 +0100

    Refactor print of status

--- src/cli/status.rs
+++ src/cli/status.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use chrono::{Local, TimeZone};
+use chrono::{DateTime, Local, TimeZone};
 
 use crate::{cli::file_utils, result::ResultWrap};
 
@@ -21,24 +21,32 @@ pub fn status() -> ResultWrap<()> {
         let start: i64 = start.parse()?;
         let end: i64 = end.parse()?;
 
-        let start_date = get_date(start)?;
-
         let diff_sec = end - start;
         let diff_hour = (diff_sec as f64) / 3600.0;
         total_secs += diff_sec;
 
-        println!("{start_date}  -  {diff_hour:.2}h");
+        print_entry(start, end, diff_hour)?;
     }
     let total_hours = (total_secs as f64) / 3600.0;
     println!("Total time: {total_hours:.2}h");
     Ok(())
 }
 
-fn get_date(timestamp: i64) -> ResultWrap<String> {
+fn print_entry(start: i64, end: i64, diff_hour: f64) -> ResultWrap<()> {
+    let start_date = get_date(start)?;
+    let end_date = get_date(end)?;
+    let current_date_format = start_date.format("%d.%m.%Y").to_string();
+    let current_week_day = start_date.format("%A").to_string();
+    let start_clock = start_date.format("%H:%M:%S").to_string();
+    let end_clock = end_date.format("%H:%M:%S").to_string();
+
+    println!("{current_date_format}  -  {diff_hour:.2}h    {start_clock}  -  {end_clock}    {current_week_day}");
+    Ok(())
+}
+
+fn get_date(timestamp: i64) -> ResultWrap<DateTime<Local>> {
     Ok(Local
         .timestamp_opt(timestamp, 0)
         .single()
-        .ok_or("Failed to convert to date")?
-        .format("%d.%m.%Y")
-        .to_string())
+        .ok_or("Failed to convert to date")?)
 }
